d5c1540a93b0081e7f7cba8de247dba908692ae5
Asliddin <asliddin750750@gmail.com>
2021-12-16 23:38:53 +0500

    file, file2, file3

---
+++ src/router/file3.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        path: 'view/phones',
+        name: 'file3.view_phones',
+        component: () => import('../views/file3/ViewPhones.vue')
+    }
+]
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import account from "./account";
+import file from "./file";
+import file2 from "./file2";
+import file3 from "./file3";
 import store from "@/store";
 
 const routes = [
@@ -12,6 +15,21 @@ const routes = [
     path: '/account',
     component: () => import('../views/account/Index.vue'),
     children: account
+  },
+  {
+    path: '/file',
+    component: () => import('../views/file/Index.vue'),
+    children: file
+  },
+  {
+    path: '/file2',
+    component: () => import('../views/file2/Index.vue'),
+    children: file2
+  },
+  {
+    path: '/file3',
+    component: () => import('../views/file3/Index.vue'),
+    children: file3
   }
 ]
 
